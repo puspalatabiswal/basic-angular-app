fb7cae3cc0b475016a247727944927a5345059a1
lalithalnt <lalitha445@gmail.com>
2017-05-17 11:52:54 +0530

    my third commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.controller("myController",["$scope",function($scope)
 	$scope.employeeList=[
 {name:"pooja" , city:"bangalore" ,phoneno:8896325478},
 {name:"rooja" , city:"ngalore" ,phoneno:8598325478},
-{name:"looja" , city:"angalore" ,phoneno:8598325478},
+{name:"looja" , city:"mangalore" ,phoneno:8598325478},
 
 
 
