211f1712d5be9bfa52186cfa63f93b037804b840
lalithalnt <lalitha445@gmail.com>
2017-05-18 16:21:17 +0530

    updated all files

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-var app = angular.module("myApp" , []);
+var app = angular.module("myApp" , ['ngRoute']);
+
+app.config(["$locationProvider", function($locationProvider)
+{
+$locationProvider.hashPreix('')
+	}])
+
+
+app.config( ["$routProvider "function($routProvider){
+$routProvider.when("/",{
+	template: "this is home page" ,
+	controller: "myController"
+})
+    .when("/register",{
+       
+	template: "this is registration page" ,
+	controller: "myController2"
+})
+
+    .when("/login",{
+       
+	template: "this is login page" ,
+	controller: "myController3"
+})
+
+}])
+app.run([function(){
+	console.log("this is un section");
+}])
+
 app.controller("myController",["$scope",function($scope)
 {
+	$scope.message="this is my home controller";
+}])
 
-	$scope.message="welcome to hyderabad";
+app.controller("myController2",["$scope",function($scope)
+{
+	$scope.message="this is my registration  controller";
+}])
 
-}]);
+app.controller("myController3",["$scope",function($scope)
+{
+	$scope.message="this is my  login controller";
+}])
